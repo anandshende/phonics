a3306c0db72139bc9598dc5a871dd2e8216fb683
Anand Shende <anandshende1994@gmail.com>
2017-11-12 01:02:27 +0530

    Server + Routes configured for application

--- server/src/route.js
+++ server/src/route.js
@@ -1,9 +1,41 @@
 module.exports = (app) => {
-    app.get('/', (req, res) => {
-        res.end('/');
+
+    // Phoneme
+    app.get('/phoneme', (req, res) => {
+        res.end('get phoneme list');
     });
 
-    app.get('/web', (req, res) => {
-        res.end('/web');
+    app.post('/phoneme', (req, res) => {
+        res.end('add phoneme');
+    });
+
+    app.put('/phoneme/:id', (req, res) => {
+        res.end('order phoneme');
+    });
+
+    app.delete('/phoneme/:id', (req, res) => {
+        res.end('delete phoneme');
+    });
+
+
+    // Words
+    app.get('/phoneme/:id', (req, res) => {
+        res.end('get words list');
+    });
+
+    app.get('/phoneme/:id/:wordId', (req, res) => {
+        res.end('get words assests');
+    });
+
+    app.post('/phoneme/:id', (req, res) => {
+        res.end('add word');
+    });
+
+    app.put('/phoneme/:id/:wordId', (req, res) => {
+        res.end('order words');
+    });
+
+    app.delete('/phoneme/:id/:wordId', (req, res) => {
+        res.end('delete word');
     });
 };
