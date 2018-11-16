06389e704e3c0f12a2416b6c9bce4721b6ce565a
ANAND\anand_000 <anandshende1994@gmail.com>
2018-11-17 01:42:35 +0530

    Search View - Grunt Changes

--- GruntFile.js
+++ GruntFile.js
@@ -10,6 +10,10 @@ module.exports = function (grunt) {
                 src: lengthBasedJSFiles,
                 dest: 'web2/assets/js/lengthBasedViews.js'
             },
+            searchView: {
+                src: searchViewJSFiles,
+                dest: 'web2/assets/js/searchView.js'
+            },
             options: {
                 process: function (src, filepath) {
                     return '\n\n' + '// ----- ----- ' + filepath + ' ----- ----- \n\n' + src;
@@ -33,10 +37,21 @@ module.exports = function (grunt) {
 
 };
 
-var lengthBasedJSFiles = [
+var CommonJSFiles = [
     'web2/assets/js/common/**/*.js',
     'web2/assets/js/vendor/**/*.js',
+];
+
+var lengthBasedJSFiles = [
+    ...CommonJSFiles,
     'web2/assets/js/features/length-based-views.js',
     'web2/assets/js/models/word-model.js',
     'web2/assets/js/services/phonics-service.js'
-]
+];
+
+var searchViewJSFiles = [
+    ...CommonJSFiles,
+    'web2/assets/js/features/search-view-init.js',
+    'web2/assets/js/models/word-model.js',
+    'web2/assets/js/services/phonics-service.js'
+];
