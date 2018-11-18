db8eb195b6178b2c6223bac4cdbd37bfb920e198
ANAND\anand_000 <anandshende1994@gmail.com>
2018-11-18 19:47:07 +0530

    Batch files for DEMO + DEV

---
+++ demo.bat
@@ -0,0 +1,7 @@
+start cmd /k "node server\src\server.js"
+
+start cmd /k "http-server .\web2\"
+
+start chrome http://localhost:8080
+
+exit
---
+++ dev.bat
@@ -0,0 +1,9 @@
+start cmd /k "call demo.bat"
+
+start cmd /k "cd .\web2\assets && call ^"sass batch.bat^" "
+
+start cmd /k "echo grunt&grunt"
+
+code .
+
+exit
