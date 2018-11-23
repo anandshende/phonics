2688466c509e9bcd563e374c51ebd077131a8aee
ANAND\anand_000 <anandshende1994@gmail.com>
2018-11-23 18:53:00 +0530

    issues

--- demo.bat
+++ demo.bat
@@ -2,6 +2,6 @@ start cmd /k "node server\src\server.js"
 
 start cmd /k "http-server .\web2\"
 
-start chrome http://localhost:8080
+start chrome http://localhost:8080/searchViews/searchPhoneme.html
 
 exit
